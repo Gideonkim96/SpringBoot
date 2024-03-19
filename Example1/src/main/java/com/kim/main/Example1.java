6cd7c08f31dc9e3cbea93e2b37922f9f90f01651
Gideonkim96 <ronogideon97@gmail.com>
2024-03-19 13:04:43 +0300

    Intro

---
+++ Example1/src/main/java/com/kim/main/Example1.java
@@ -0,0 +1,25 @@
+package com.kim.main;
+
+import com.kim.beans.Vehicle;
+import com.kim.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Example1 {
+
+    public static void main(String[] args) {
+        Vehicle vehicle = new Vehicle();
+        vehicle.setName("Honda City");
+        System.out.println("Vehicle name from non-spring context is: " + vehicle.getName());
+
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Vehicle veh = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring Context is: " + veh.getName());
+
+        String hello = context.getBean(String.class);
+        System.out.println("String value from Spring Context is: " + hello);
+        Integer num = context.getBean(Integer.class);
+        System.out.println("Integer value from Spring Context is: " + num);
+    }
+}
